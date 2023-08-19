765ae110ff7721d1444b99f94f54d823985c5954
Milos Savkovic <milos.savkovic.somias@gmail.com>
2023-08-19 14:33:21 +0200

    Initial Commit

---
+++ ThreadsTutorial/ThreadsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialApp.swift
+//  ThreadsTutorial
+//
+//  Created by Milos on 19. 8. 2023..
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
