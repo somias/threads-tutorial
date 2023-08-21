da24ce5811a0f4b5c0ea117b302d906190447c75
Milos Savkovic <milos.savkovic.somias@gmail.com>
2023-08-21 18:25:03 +0200

    - Create Thread UI

--- ThreadsTutorial/Core/TabBar/ThreadsTabView.swift
+++ ThreadsTutorial/Core/TabBar/ThreadsTabView.swift
@@ -8,33 +8,57 @@
 import SwiftUI
 
 struct ThreadsTabView: View {
+    @State private var selectedTab = 0
+    @State private var showCreateThreadView = false
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             FeedView()
                 .tabItem {
                     Image(systemName: "house")
                 }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
             
             ExploreView()
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                 }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
             
-            CreateThreadView()
+            Text("")
                 .tabItem {
                     Image(systemName: "plus")
                 }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
             
             ActivityView()
                 .tabItem {
                     Image(systemName: "heart")
                 }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
             
             ProfileView()
                 .tabItem {
                     Image(systemName: "person")
                 }
-        }.tint(.black)
+                .onAppear { selectedTab = 4 }
+                .tag(4)
+            
+        }
+        .onChange(of: selectedTab, perform: { _ in
+            showCreateThreadView = selectedTab == 2
+        })
+        // Also I need to call "dismiss" in CreateThreadView if I want to cancel the view
+        .sheet(isPresented: $showCreateThreadView, onDismiss: {
+            selectedTab = 0
+        }, content: {
+            CreateThreadView()
+        })
+        .tint(.black)
     }
 }
 
--- ThreadsTutorial/ThreadCreation/Views/CreateThreadView.swift
+++ ThreadsTutorial/ThreadCreation/Views/CreateThreadView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    @State private var newThread = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImageView()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("charles_leclerc")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Start a thread", text: $newThread, axis: .vertical)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if !newThread.isEmpty {
+                        Button {
+                            newThread = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                        }
+                        
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .disabled(newThread.isEmpty)
+                    .opacity(newThread.isEmpty ? 0.5 : 1.0)
+                    .fontWeight(.semibold)
+                    
+                }
+            }
+            .font(.subheadline)
+            .foregroundColor(.black)
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
