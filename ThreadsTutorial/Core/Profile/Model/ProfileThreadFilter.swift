85243ec5b1ecb51e88dfe7d9563c187776c10457
Milos Savkovic <milos.savkovic.somias@gmail.com>
2023-08-21 14:49:26 +0200

    - Profile View UI: Bio and stats - Create Model of ProfileThreadFilter

---
+++ ThreadsTutorial/Core/Profile/Model/ProfileThreadFilter.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileThreadFilter.swift
+//  ThreadsTutorial
+//
+//  Created by Milos on 21. 8. 2023..
+//
+
+import Foundation
+
+enum ProfileThreadFilter: Int, CaseIterable, Identifiable {
+    case threads
+    case replies
+    
+    var title: String {
+        switch self {
+        case .threads: return "Threads"
+        case .replies: return "Replies"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
--- ThreadsTutorial/Core/Profile/Views/ProfileView.swift
+++ ThreadsTutorial/Core/Profile/Views/ProfileView.swift
@@ -8,8 +8,77 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var selectedFilter: ProfileThreadFilter = .threads
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: false) {
+            // bio and stats
+            VStack(spacing: 20) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        // fullname and username
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Charles Leclerc")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            
+                            Text("charles_leclerc")
+                                .font(.subheadline)
+                        }
+                        
+                        Text("Formula 1 driver for Scuderia Ferrari")
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Text("2 followers")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                    }
+                    Spacer()
+                    
+                    CircularProfileImageView()
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 32)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                
+                // user content list view
+                VStack {
+                    HStack {
+                        ForEach(ProfileThreadFilter.allCases) { filter in
+                            VStack {
+                                Text(filter.title)
+                                    .font(.subheadline)
+                                    .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                                
+                                if selectedFilter == filter {
+                                    Rectangle()
+                                        .foregroundColor(.black)
+                                        .frame(width: 180, height: 1)
+                                } else {
+                                    Rectangle()
+                                        .foregroundColor(.clear)
+                                        .frame(width: 180, height: 1)
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+           
+        }
+        .padding(.horizontal)
     }
 }
 
